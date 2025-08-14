c83238db7fa5823f99a85e7734e7d173607870a6
miloshkens <miloshkekg@gmail.com>
2025-08-14 08:25:12 -0400

    message

--- calculateArea/calculateArea.js
+++ calculateArea/calculateArea.js
@@ -1,8 +1,6 @@
 function calculateArea() {
-    let length;
-    let width;
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
-let area = length * width;
-document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+    const length = parseFloat(document.getElementById('length').value);
+    const width = parseFloat(document.getElementById('width').value);
+    const area = length * width;
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
